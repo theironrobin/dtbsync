922b734cbb09a4935a5a24d231445fd5741dc94d
Maud Spierings <maud_spierings@hotmail.com>
2026-07-31 22:58:01 +0200

    add asus vivobook s15 x elite

--- dtbsync/cli.py
+++ dtbsync/cli.py
@@ -21,6 +21,7 @@ dtbMap = {
     "orangepi-5-plus": ("rockchip", "rk3588-orangepi-5-plus.dtb"),
     "thinkpad-t14s-oled": ("qcom", "x1e78100-lenovo-thinkpad-t14s-oled.dtb"),
     "thinkpad-x13s": ("qcom", "sc8280xp-lenovo-thinkpad-x13s.dtb"),
+    "vivobook-s15": ("qcom", "x1e80100-asus-vivobook-s15.dtb"),
 }
 
 
